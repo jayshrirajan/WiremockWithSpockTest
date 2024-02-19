3e09036c2978c930413a943878397f87e051ce45
Jayasri Rajan <Jayasri.Rajan@bakkt.com>
2024-02-19 14:03:56 +0530

    final commit

--- WireMockAndSpockTest/src/main/java/com/example/WireMock/config/ItemConfig.java
+++
@@ -1,2 +0,0 @@
-package com.example.WireMock.config;public class ItemConfig {
-}
--- WireMockAndSpockTest/src/main/java/com/example/WireMock/exception/UnhandledException.java
+++
@@ -1,8 +0,0 @@
-package com.example.WireMock.exception;
-
-public class UnhandledException extends RuntimeException {
-
-    public UnhandledException(String message) {
-        super(message);
-    }
-}
